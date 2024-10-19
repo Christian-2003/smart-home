ad7cba33b17a9feb5cf417a81e272faafab9db4d
Christian-2003 <80711177+Christian-2003@users.noreply.github.com>
2024-10-19 17:57:49 +0200

    Changes to UI

--- app/src/main/java/de/christian2003/smarthome/view/home/HomeFragment.java
+++ app/src/main/java/de/christian2003/smarthome/view/home/HomeFragment.java
@@ -1,19 +1,13 @@
 package de.christian2003.smarthome.view.home;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import de.christian2003.smarthome.R;
 import de.christian2003.smarthome.utils.framework.SmartHomeFragment;
-import de.christian2003.smarthome.view.url.UrlActivity;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 
 /**
@@ -45,54 +39,7 @@ public class HomeFragment extends SmartHomeFragment<HomeViewModel> {
             return null;
         }
 
-        view.findViewById(R.id.button_url).setOnClickListener(v -> {
-            Intent intent = new Intent(getActivity(), UrlActivity.class);
-            intent.putExtra(UrlActivity.EXTRA_CLOSEABLE, true);
-            startActivity(intent);
-        });
-
         return view;
-    }
-
-
-    /**
-     * TODO: Remove this.
-     * @param view
-     */
-    public void GetData(View view) {
-        // Check that button on click works.
-        TextView tvHeadline = fragmentView.findViewById(R.id.tvHeadline);
-        tvHeadline.setText("Button clicked");
-
-        // Get data form the website.
-        new Thread(() -> {
-            OkHttpClient client = new OkHttpClient();
-
-            Request request = new Request.Builder()
-                    .url("https://www.google.com")
-                    .build();
-
-            try{
-                Response response = client.newCall(request).execute();
-
-                if (response.isSuccessful() && response.body() != null) {
-                    String htmlContent = response.body().string();
-                    response.close();
-
-                    requireActivity().runOnUiThread(() -> {
-                        TextView tvData = fragmentView.findViewById(R.id.tvData);
-                        tvData.setText(htmlContent);
-                        System.out.println("HTML Content: " + htmlContent);
-                    });
-                }
-                else {
-                    System.out.println("Error body might be null");
-                }
-            }
-            catch (Exception exception) {
-                System.out.println("Exception: " + exception.getMessage());
-            }
-        }).start();
     }
 
 }
--- app/src/main/java/de/christian2003/smarthome/view/settings/SettingsFragment.java
+++ app/src/main/java/de/christian2003/smarthome/view/settings/SettingsFragment.java
@@ -1,7 +1,15 @@
 package de.christian2003.smarthome.view.settings;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import de.christian2003.smarthome.R;
 import de.christian2003.smarthome.utils.framework.SmartHomeFragment;
+import de.christian2003.smarthome.view.url.UrlActivity;
 
 
 /**
@@ -14,6 +22,41 @@ public class SettingsFragment extends SmartHomeFragment<SettingsViewModel> {
      */
     public SettingsFragment() {
         super(SettingsViewModel.class, R.layout.fragment_settings);
+    }
+
+
+    /**
+     * Method is called whenever the view for the fragment is created.
+     *
+     * @param inflater              Layout inflater to use to inflate the fragment layout.
+     * @param container             Parent for the fragment view.
+     * @param savedInstanceState    Previously saved state of the instance.
+     * @return                      Created view.
+     */
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        if (view == null) {
+            return null;
+        }
+
+        view.findViewById(R.id.container_url).setOnClickListener(this::startUrlActivity);
+
+        return view;
+    }
+
+
+    /**
+     * Method starts the activity through which to edit the server URL, once the corresponding view
+     * is clicked.
+     *
+     * @param view  View that was clicked.
+     */
+    private void startUrlActivity(View view) {
+        Intent intent = new Intent(getActivity(), UrlActivity.class);
+        intent.putExtra(UrlActivity.EXTRA_CLOSEABLE, true);
+        startActivity(intent);
     }
 
 }
