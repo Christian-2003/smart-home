4be8dc2fc4fdbac539bdcdf2a4f81e86e6326eff
Christian-2003 <80711177+Christian-2003@users.noreply.github.com>
2025-01-17 19:34:04 +0100

    General state showed on main screen in car app

--- common/auto/src/main/java/de/christian2003/smarthome/auto/screen/MainScreen.kt
+++ common/auto/src/main/java/de/christian2003/smarthome/auto/screen/MainScreen.kt
@@ -7,6 +7,7 @@ import androidx.car.app.model.ItemList
 import androidx.car.app.model.ListTemplate
 import androidx.car.app.model.Row
 import androidx.car.app.model.Template
+import de.christian2003.smarthome.data.model.room.ShInfoText
 import de.christian2003.smarthome.data.model.room.ShRoom
 
 
@@ -26,8 +27,15 @@ class MainScreen(
     override fun onGetTemplate(): Template {
         val builder = ItemList.Builder()
 
-        rooms.forEach { room ->
-            builder.addItem(buildRow(room))
+        if (rooms.isNotEmpty()) {
+            rooms[0].infos.forEach { infoText ->
+                builder.addItem(buildRow(infoText))
+            }
+        }
+        if (rooms.size >= 2) {
+            rooms.subList(1, rooms.size).forEach { room ->
+                builder.addItem(buildRow(room))
+            }
         }
 
         return ListTemplate.Builder()
@@ -47,9 +55,26 @@ class MainScreen(
     private fun buildRow(room: ShRoom): Row {
         val builder = Row.Builder()
         builder.setTitle(room.name)
+        builder.addText(carContext.baseContext.getString(de.christian2003.smarthome.data.R.string.car_details_subtext))
         builder.setOnClickListener {
             screenManager.push(RoomScreen(carContext, room))
         }
+        return builder.build()
+    }
+
+    /**
+     * Method creates a row for an info text.
+     *
+     * @param infoText  Info text for which to create the row.
+     * @return          Row for the info text specified.
+     */
+    private fun buildRow(infoText: ShInfoText): Row {
+        var title = carContext.baseContext.getString(de.christian2003.smarthome.data.R.string.car_main_text)
+        title = title.replace("{label}", infoText.label)
+        title = title.replace("{text}", if (infoText.text != null) { infoText.text!! } else { "" })
+
+        val builder = Row.Builder()
+        builder.setTitle(title)
         return builder.build()
     }
 
