eae1ef5d0a1a737cde542df5c227c837f90a9cf0
Christian-2003 <80711177+Christian-2003@users.noreply.github.com>
2025-03-04 16:21:49 +0100

    Release version 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "de.christian2003.smarthome"
         minSdk = 33
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
